b181666b595d77529ce77e1f74fc7cad52350e74
FelixYosa <felix_yosa@ymail.com>
2016-05-11 21:51:18 +0700

    edit

---
+++ src/tugas/bab7/Gold.java
@@ -0,0 +1,46 @@
+//Nama : Felix Yosa H. S.
+//NIM : 155150400111129
+//Kelas : SI-H
+
+package mainsalon;
+
+public class Gold extends Salon {
+
+    private double jasa, barang;
+
+    public Gold(String Nama, String Member, double jasa, double barang) {
+        super(Nama, Member);
+        setJasa(jasa);
+        setBarang(barang);
+    }
+
+    public void setJasa(double jasa) {
+        this.jasa = jasa;
+    }
+
+    public double getJasa() {
+        return jasa;
+    }
+
+    public void setBarang(double barang) {
+        this.barang = barang;
+    }
+
+    public double getBarang() {
+        return barang;
+    }
+
+    @Override
+    public double diskon() {
+        return (getJasa() * 15 / 100) + (getBarang() * 10 / 100);
+    }
+
+    @Override
+    public double Total() {
+        return getJasa() + getBarang() - diskon();
+    }
+
+    public String getNota() {
+        return (super.getNota() + "\nBiaya\t: " + getJasa() + "\nBarang\t: " + getBarang() + "\nDiskon\t: " + diskon() + "\nTotal\t: " + Total());
+    }
+}
